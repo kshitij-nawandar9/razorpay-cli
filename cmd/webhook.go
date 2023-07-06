ccd39fb1e55c8da315c326480ba5d9f3de5903bc
manask322 <manask322@gmail.com>
2023-07-07 00:16:36 +0530

    rename

--- cmd/webhook.go
+++ cmd/webhook.go
@@ -8,8 +8,8 @@ import (
 
 var webhookCmd = &cobra.Command{
 	Use:   "webhook",
-	Short: "webhook testing",
-	Long:  `webhook testing`,
+	Short: "merchant webhook",
+	Long:  `merchant webhook`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		fmt.Println("Webhook command")
