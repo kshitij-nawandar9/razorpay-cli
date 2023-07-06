b3782c8301487c59aa0b92cf537add0b48a336a2
manask322 <manask322@gmail.com>
2023-07-06 22:45:45 +0530

    reformat

--- cmd/request.go
+++ cmd/request.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
 func makeRequest(_ context.Context, uri string, method string, payload interface{}) (interface{}, error) {
@@ -22,13 +21,14 @@ func makeRequest(_ context.Context, uri string, method string, payload interface
 	}
 
 	headers := map[string]string{
-		"Content-Type":  "application/json",
-		"Authorization": os.Getenv("BASIC_AUTH"),
+		"Content-Type": "application/json",
 	}
 
 	for key, value := range headers {
 		req.Header.Set(key, value)
 	}
+
+	req.SetBasicAuth("rzp_live_ruiXfILw0kpEXc", "X5i5mEBSVn82de7DoP3SUBRx")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
