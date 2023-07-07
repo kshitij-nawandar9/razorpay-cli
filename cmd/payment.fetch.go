148e48fee20e695b88e44834974728c7ab625f7a
manask322 <manask322@gmail.com>
2023-07-07 11:02:06 +0530

    some

--- cmd/payment.fetch.go
+++ cmd/payment.fetch.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/spf13/cobra"
 	"log"
 	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/stripe/stripe-cli/pkg/ansi"
 )
 
 var paymentId string
@@ -20,6 +23,9 @@ var paymentFetchCmd = &cobra.Command{
 	Short: "Fetch Payment by Id",
 	Long:  "Fetch Payment by Id",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		s := ansi.StartNewSpinner("fetching payment ...", os.Stdout)
+		time.Sleep(1 * time.Second)
 		method := "GET"
 		payload := []byte(``)
 
@@ -33,6 +39,7 @@ var paymentFetchCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal(err)
 		}
+		ansi.StopSpinner(s, "DONE!", os.Stdout)
 		fmt.Println(string(val))
 	},
 }
